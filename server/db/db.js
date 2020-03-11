e96c9263c81e583021d5cb84628b79aa90cc82a4
soso01 <6125024@gmail.com>
2020-03-11 14:57:26 +0900

    heroku mongodb setting

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
-mongoose.connect('mongodb://localhost:27017/netparty', { useNewUrlParser: true })
+//mongoose.connect('mongodb://localhost:27017/netparty', { useNewUrlParser: true })
+mongoose.connect('mongodb://heroku_fdd65p9g:9d7tmr91n8kuo3279s94nfqda9@ds219078.mlab.com:19078/heroku_fdd65p9g', { useNewUrlParser: true })
 
 const db = mongoose.connection
 
@@ -11,4 +12,4 @@ db.once('open', () => {
   console.log("db 접속성공")
 })
 
-module.exports = db
+module.exports = db 
